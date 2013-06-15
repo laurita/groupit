6dca93f574168bd7a71f64ec5fb8ec7b99fb2a62
laura <laura.bledaite@gmail.com>
2013-06-15 12:55:07 +0200

    Init.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Groupit::Application.config.secret_token = '6ec768db1b7828adb7fc75ae5b66f472433d77b626e0331b1f73f849a7749fb0bfeab9f077ce357766ac71b878a49eadb3913f4f98392f777b19e8735d509391'
